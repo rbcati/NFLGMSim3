79b5a048944347225d9ac4a0aa309797b44b5547
rbcati <68180584+rbcati@users.noreply.github.com>
2025-08-08 15:13:11 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
+// src/App.js
+import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import RosterPage from './components/RosterPage';
 import StatsPage from './components/StatsPage';
 import GameSimPage from './components/GameSimPage';
 
+const HomePage = () => <div style={{ padding: '20px' }}><h1>Welcome to NFL GM Simulator</h1></div>;
+
 function App() {
   return (
     <Router>
+      <Navbar />
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/roster" element={<RosterPage />} />
         <Route path="/stats" element={<StatsPage />} />
         <Route path="/simulate" element={<GameSimPage />} />
-        {/* Add other routes as needed */}
       </Routes>
     </Router>
   );
