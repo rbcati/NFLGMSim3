173136bf57b99434b5dec3d22d23fdd03b0721c5
rbcati <68180584+rbcati@users.noreply.github.com>
2025-08-08 17:06:15 -0700

    Create react-router-dom.js

---
+++ react-router-dom.js
@@ -0,0 +1,27 @@
+// src/App.js
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import Roster from './components/Roster';
+import TradeCenter from './components/TradeCenter';
+import Draft from './components/Draft';
+import FreeAgency from './components/FreeAgency';
+import Season from './components/Season';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/roster" element={<Roster />} />
+        <Route path="/trade" element={<TradeCenter />} />
+        <Route path="/draft" element={<Draft />} />
+        <Route path="/free-agency" element={<FreeAgency />} />
+        <Route path="/season" element={<Season />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
