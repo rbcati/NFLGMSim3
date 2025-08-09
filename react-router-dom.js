9d87fc61314bca6e12c111e5e260511513496fb7
rbcati <68180584+rbcati@users.noreply.github.com>
2025-08-08 17:07:50 -0700

    Update react-router-dom.js

--- react-router-dom.js
+++ react-router-dom.js
@@ -25,3 +25,20 @@ function App() {
 }
 
 export default App;
+
+// src/components/Season.js (or any component)
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+
+const Season = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div>
+      <Button variant="outlined" onClick={() => navigate(-1)}>
+        Back
+      </Button>
+      {/* Season content */}
+    </div>
+  );
+};
